91b44634352911a755af860e3be15bfd95b7ef8f
harrypik2 <ssharry2k3ss@gmail.com>
2023-10-05 22:01:14 +0700

    setup project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<h1>Hello world</h1>
+<h2>tôi là phúc</h2>
+</body>
+</html>
